3ee6885f2f3647c16e5fdda9c262f2911fd85425
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-05 22:59:45 +0530

    Create basicProblems.cpp

---
+++ basicProblems.cpp
@@ -0,0 +1,16 @@
+
+
+                                                          //  Valid Perfect Square   //
+                                                          
+                                                         
+ class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int squareRoot=sqrt(num);
+        cout<<squareRoot<<endl;
+        if(squareRoot*squareRoot==num)
+        return true;
+        else
+        return false;
+    }
+};
