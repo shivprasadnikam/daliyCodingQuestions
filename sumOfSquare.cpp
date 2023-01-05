de61a661283209505953f4ff860bcd9af3bb885d
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-05 23:24:55 +0530

    Create sumOfSquare.cpp

---
+++ sumOfSquare.cpp
@@ -0,0 +1,21 @@
+
+
+                                                        // Sum of square //
+
+
+
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(long long i=0; i*i<=c; i++)
+        {
+            double j = sqrt(c-i*i);
+            
+            
+           if(j==(int)j)
+            return true;
+        }
+        return false;
+    }
+};
