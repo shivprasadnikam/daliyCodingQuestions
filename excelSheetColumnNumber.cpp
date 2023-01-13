a21648c7c0f85237ad1bffe121373bf902e1027e
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-13 13:53:07 +0530

    Create excelSheetColumnNumber.cpp

---
+++ excelSheetColumnNumber.cpp
@@ -0,0 +1,22 @@
+
+
+                                                              //  Excel Sheet Column Number  //
+
+
+
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int result=0;
+        for(int i=0; i<columnTitle.size(); i++)
+        {
+            result*=26;
+            result+=(columnTitle[i]-'A'+1);
+        }
+        return result;
+    }
+};
+
+
+
